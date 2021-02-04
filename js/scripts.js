ea9b26f99d834e880322a5f78cf50cda7d18885b
Justin Goats <justingoats@yahoo.com>
2021-02-04 07:05:34 -0800

    added pokemon list to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,23 @@
-alert("hello world");
-var favoriteFood = "Pizza";
-document.write(favoriteFood);
+var pokemonList = [
+    {
+        name : "charmander",
+        height : 2,
+        types : ["fire"]
+    },
+    {
+        name : "snorlax",
+        height : 6.11,
+        type : ["normal"]
+    },
+    {
+        name : "combusken",
+        height : 2.11,
+        types : ["fire" , "fighting"]
+    },
+    {
+        name : "torkoal",
+        height : 1.8,
+        types : ["fire"]
+    }
+
+];
